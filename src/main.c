c311b6e5861f516408f0e049ccc5d0cc8f437762
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2025-11-07 17:05:54 +0100

    Updated main.c to beta-6 version

--- src/main.c
+++ src/main.c
@@ -10,7 +10,84 @@
 #include "signals.h"
 #include "linenoise.h"
 
-int main() {
+static void load_profile(const char *path) {
+    if (access(path, R_OK) == 0) {
+        char cmd[1024];
+        snprintf(cmd, sizeof(cmd), "sh %s", path);
+        system(cmd);
+    }
+}
+
+static void process_command_line(char *line) {
+    char *seq_cmds[16];
+    int seq_count = 0;
+    char *saveptr1;
+
+    char *part = strtok_r(line, "&&", &saveptr1);
+    while (part && seq_count < 16) {
+        while (*part == ' ') part++;
+        char *end = part + strlen(part) - 1;
+        while (end > part && *end == ' ') *end-- = '\0';
+        seq_cmds[seq_count++] = part;
+        part = strtok_r(NULL, "&&", &saveptr1);
+    }
+
+    for (int i = 0; i < seq_count; i++) {
+        char *pipe_cmds[16];
+        int pipe_count = 0;
+        char *saveptr2;
+
+        char *p = strtok_r(seq_cmds[i], "|", &saveptr2);
+        while (p && pipe_count < 16) {
+            while (*p == ' ') p++;
+            char *end = p + strlen(p) - 1;
+            while (end > p && *end == ' ') *end-- = '\0';
+            pipe_cmds[pipe_count++] = p;
+            p = strtok_r(NULL, "|", &saveptr2);
+        }
+
+        if (pipe_count > 1) {
+            execute_pipeline(pipe_cmds, pipe_count);
+        } else {
+            exec_command(pipe_cmds[0]);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 1 &&
+        (strcmp(argv[1], "--version") == 0 ||
+         strcmp(argv[1], "-v") == 0 ||
+         strcmp(argv[1], "-version") == 0)) {
+
+        printf("CVX Shell beta-6\n");
+        printf("Copyright (C) 2025 JHX Studio's\n");
+        printf("License: GNU GPL v3.0\n");
+        return 0;
+    }
+
+    if (argc > 2 && strcmp(argv[1], "-c") == 0) {
+        char cmd[4096] = {0};
+        for (int i = 2; i < argc; i++) {
+            strncat(cmd, argv[i], sizeof(cmd) - strlen(cmd) - 1);
+            if (i < argc - 1)
+                strncat(cmd, " ", sizeof(cmd) - strlen(cmd) - 1);
+        }
+    
+        process_command_line(cmd);
+        return 0;
+    }    
+
+    if (argc > 1 && strcmp(argv[1], "-l") == 0) {
+        load_profile("/etc/profile");
+        const char *home = getenv("HOME");
+        if (home) {
+            char user_profile[1024];
+            snprintf(user_profile, sizeof(user_profile), "%s/.profile", home);
+            load_profile(user_profile);
+        }
+    }
+
     char *line;
     signal(SIGINT, sigint_handler);
     setenv("TERM", "xterm", 1);
@@ -19,7 +96,9 @@ int main() {
 
     const char *history_file = ".cvx_history";
     char history_path[1024];
-    snprintf(history_path, sizeof(history_path), "%s/%s", getenv("HOME"), history_file);
+    const char *home = getenv("HOME");
+    if (!home) home = ".";
+    snprintf(history_path, sizeof(history_path), "%s/%s", home, history_file);
     linenoiseHistoryLoad(history_path);
     getcwd(current_dir, sizeof(current_dir));
 
@@ -38,43 +117,9 @@ int main() {
         if (line[0] != '\0' && history_enabled) {
             linenoiseHistoryAdd(line);
             linenoiseHistorySave(history_path);
-        }        
-
-        char *seq_cmds[16];
-        int seq_count = 0;
-        char *part = strtok(line, "&&");
-        while (part && seq_count < 16) {
-            while (*part == ' ') part++;
-            char *end = part + strlen(part) - 1;
-            while (end > part && (*end == ' ')) *end-- = '\0';
-            seq_cmds[seq_count++] = part;
-            part = strtok(NULL, "&&");
         }
 
-        for (int i = 0; i < seq_count; i++) {
-            char *pipe_cmds[16];
-            int pipe_count = 0;
-            char *p = strtok(seq_cmds[i], "|");
-            while (p && pipe_count < 16) {
-                while (*p == ' ') p++;
-                char *end = p + strlen(p) - 1;
-                while (end > p && (*end == ' ')) *end-- = '\0';
-                pipe_cmds[pipe_count++] = p;
-                p = strtok(NULL, "|");
-            }
-
-            if (pipe_count > 1) {
-                if (fork() == 0) {
-                    execute_pipeline(pipe_cmds, pipe_count);
-                    exit(0);
-                } else {
-                    int status;
-                    wait(&status);
-                }
-            } else {
-                exec_command(pipe_cmds[0]);
-            }
-        }
+        process_command_line(line);
 
         free(line);
     }
