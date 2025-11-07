170fabc661db952e975b01ee435134d688396074
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2025-11-07 23:03:22 +0100

    Created commands.h

---
+++ src/commands.h
@@ -0,0 +1,11 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+int cmd_cd(int argc, char **argv);
+int cmd_pwd(int argc, char **argv);
+int cmd_echo(int argc, char **argv);
+int cmd_export(int argc, char **argv);
+int cmd_help(int argc, char **argv);
+int cmd_history(int argc, char **argv);
+
+#endif
