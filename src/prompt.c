8277704aca96919dba0bde331b5f4332757f8396
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2025-11-08 22:06:50 +0100

    Updated prompt.c to beta-8 version.

--- src/prompt.c
+++ src/prompt.c
@@ -3,31 +3,55 @@
 #include <string.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <ctype.h>
 #include "prompt.h"
 
-void print_prompt() {
-    char home[1024];
-    struct passwd *pw = getpwuid(getuid());
-    const char *username = pw ? pw->pw_name : "unknown";
-
-    char hostname[256];
-    gethostname(hostname, sizeof(hostname));
-
-    strncpy(home, getenv("HOME") ? getenv("HOME") : "", sizeof(home));
-    home[sizeof(home)-1] = '\0';
-
-    if (strncmp(current_dir, home, strlen(home)) == 0) {
-
-        printf("%s@%s:~%s$ ", username, hostname, current_dir + strlen(home));
-    } else {
-        printf("%s@%s:%s$ ", username, hostname, current_dir);
+static void expand_escapes(const char *src, char *dest, size_t dest_size) {
+    size_t j = 0;
+    for (size_t i = 0; src[i] && j < dest_size - 1; i++) {
+        if (src[i] == '\\' && src[i+1]) {
+            i++;
+            switch (src[i]) {
+                case 'n': dest[j++] = '\n'; break;
+                case 't': dest[j++] = '\t'; break;
+                case 'r': dest[j++] = '\r'; break;
+                case 'a': dest[j++] = '\a'; break;
+                case 'v': dest[j++] = '\v'; break;
+                case '\\': dest[j++] = '\\'; break;
+                case '"': dest[j++] = '"'; break;
+                case '$': dest[j++] = '$'; break;
+                case 'x': {
+                    i++;
+                    if (isxdigit(src[i])) {
+                        int val = 0;
+                        for (int k = 0; k < 2 && isxdigit(src[i]); k++, i++) {
+                            char c = src[i];
+                            val *= 16;
+                            if (c >= '0' && c <= '9') val += c - '0';
+                            else if (c >= 'a' && c <= 'f') val += c - 'a' + 10;
+                            else if (c >= 'A' && c <= 'F') val += c - 'A' + 10;
+                        }
+                        i--;
+                        dest[j++] = (char)val;
+                    } else {
+                        dest[j++] = 'x';
+                        i--;
+                    }
+                    break;
+                }
+                default: dest[j++] = src[i]; break;
+            }
+        } else {
+            dest[j++] = src[i];
+        }
     }
+    dest[j] = '\0';
 }
 
 const char* get_prompt(void) {
     static char prompt[2048];
 
-    if (strcmp(cvx_prompt, "DEFAULT_PROMPT") == 0) {
+    if (!cvx_prompt || strcmp(cvx_prompt, "DEFAULT_PROMPT") == 0 || cvx_prompt[0] == '\0') {
         char home[1024], hostname[256];
         struct passwd *pw = getpwuid(getuid());
         const char *username = pw ? pw->pw_name : "unknown";
@@ -39,34 +63,64 @@ const char* get_prompt(void) {
             snprintf(prompt, sizeof(prompt), "%s@%s:~%s$ ", username, hostname, current_dir + strlen(home));
         else
             snprintf(prompt, sizeof(prompt), "%s@%s:%s$ ", username, hostname, current_dir);
-    } else {
 
-        char buf[2048];
-        strncpy(buf, cvx_prompt, sizeof(buf)-1);
-        buf[sizeof(buf)-1] = '\0';
-        char *p = buf;
-        char tmp[2048];
-        tmp[0] = '\0';
-
-        while (*p) {
-            if (*p == '$') {
-                if (strncmp(p, "$USER", 5) == 0) {
-                    strncat(tmp, getenv("USER") ? getenv("USER") : "", sizeof(tmp)-strlen(tmp)-1);
-                    p += 5;
-                    continue;
-                } else if (strncmp(p, "$HOST", 5) == 0) {
-                    char hostname[256];
-                    gethostname(hostname, sizeof(hostname));
-                    strncat(tmp, hostname, sizeof(tmp)-strlen(tmp)-1);
-                    p += 5;
-                    continue;
-                }
-            }
-            strncat(tmp, p, 1);
-            p++;
-        }
-        snprintf(prompt, sizeof(prompt), "%.*s$ ", (int)(sizeof(prompt)-3), tmp);
+        return prompt;
     }
 
+    char tmp[2048];
+    char buf[2048] = "";
+    char expanded[2048];
+
+    strncpy(tmp, cvx_prompt, sizeof(tmp)-1);
+    tmp[sizeof(tmp)-1] = '\0';
+
+    char *p = tmp;
+    while (*p) {
+        if (*p == '$') {
+            if (strncmp(p, "$USER", 5) == 0) {
+                strncat(buf, getenv("USER") ? getenv("USER") : "", sizeof(buf)-strlen(buf)-1);
+                p += 5;
+                continue;
+            } else if (strncmp(p, "$HOST", 5) == 0) {
+                char hostname[256];
+                gethostname(hostname, sizeof(hostname));
+                strncat(buf, hostname, sizeof(buf)-strlen(buf)-1);
+                p += 5;
+                continue;
+            } else if (strncmp(p, "$PWD", 4) == 0) {
+                char pwd[1024];
+                getcwd(pwd, sizeof(pwd));
+                const char *home = getenv("HOME");
+                if (home && strncmp(pwd, home, strlen(home)) == 0) {
+                    char tmp2[1024];
+                    snprintf(tmp2, sizeof(tmp2), "~%s", pwd + strlen(home));
+                    strncat(buf, tmp2, sizeof(buf)-strlen(buf)-1);
+                } else {
+                    strncat(buf, pwd, sizeof(buf)-strlen(buf)-1);
+                }
+                p += 4;
+                continue;
+            } else {
+                char varname[128] = "";
+                int i = 0;
+                p++;
+                while ((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z') || 
+                       (*p >= '0' && *p <= '9') || *p == '_') {
+                    if (i < 127) varname[i++] = *p;
+                    p++;
+                }
+                varname[i] = '\0';
+                const char *val = getenv(varname);
+                if (val) strncat(buf, val, sizeof(buf)-strlen(buf)-1);
+                continue;
+            }
+        }
+        strncat(buf, p, 1);
+        p++;
+    }
+
+    expand_escapes(buf, expanded, sizeof(expanded));
+    snprintf(prompt, sizeof(prompt), "%s$ ", expanded);
+    
     return prompt;
 }
