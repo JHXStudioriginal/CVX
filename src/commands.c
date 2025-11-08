af0109592f012828cb2e2ba55254efaaf8f8c84c
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2025-11-08 22:07:24 +0100

    Updated commands.c to beta-8 version

--- src/commands.c
+++ src/commands.c
@@ -5,47 +5,91 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 extern char current_dir[1024];
+char previous_dir[1024] = "";
+
+static void print_with_escapes(const char *s) {
+    for (int i = 0; s[i]; i++) {
+        if (s[i] == '\\' && s[i+1]) {
+            i++;
+            switch (s[i]) {
+                case 'n': putchar('\n'); break;
+                case 't': putchar('\t'); break;
+                case 'r': putchar('\r'); break;
+                case 'a': putchar('\a'); break;
+                case 'v': putchar('\v'); break;
+                case '\\': putchar('\\'); break;
+                case '"': putchar('"'); break;
+                case '$': putchar('$'); break;
+                case 'x': {
+                    int val = 0;
+                    for (int k = 0; k < 2; k++) {
+                        char c = s[i+1];
+                        if (!c || !isxdigit(c)) break;
+                        i++;
+                        if (c >= '0' && c <= '9') val = val*16 + (c-'0');
+                        else if (c >= 'a' && c <= 'f') val = val*16 + (c-'a'+10);
+                        else if (c >= 'A' && c <= 'F') val = val*16 + (c-'A'+10);
+                    }
+                    putchar(val);
+                    break;
+                }
+                default: putchar(s[i]); break;
+            }
+        } else {
+            putchar(s[i]);
+        }
+    }
+}
+
+    
+
+int cmd_echo(int argc, char **argv) {
+    for (int i = 1; i < argc; i++) {
+        print_with_escapes(argv[i]);
+        if (i < argc - 1) putchar(' ');
+    }
+    putchar('\n');
+    return 0;
+}
 
 int cmd_cd(int argc, char **argv) {
+    char *target;
+    char cwd[1024];
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL) cwd[0] = '\0';
+
     if (argc < 2) {
-        fprintf(stderr, "cd: no argument\n");
-        return 1;
-    }
-
-    char *target = argv[1];
-    char resolved_path[1024];
-
-    if (target[0] == '~') {
-        const char *home = getenv("HOME");
-        if (!home) home = "/";
-        snprintf(resolved_path, sizeof(resolved_path), "%s%s", home, target + 1);
-        target = resolved_path;
+        target = getenv("HOME");
+        if (!target) target = "/";
+    } else if (strcmp(argv[1], "-") == 0) {
+        target = previous_dir[0] ? previous_dir : getenv("HOME");
+        if (!target) target = "/";
+        printf("%s\n", target);
+    } else {
+        target = argv[1];
     }
 
     if (chdir(target) != 0) {
-        perror("cd error");
+        perror("cd");
         return 1;
     }
 
-    if (getcwd(current_dir, sizeof(current_dir)) == NULL) {
-        perror("getcwd error");
-        current_dir[0] = '\0';
-    }
+    strncpy(previous_dir, cwd, sizeof(previous_dir)-1);
+    previous_dir[sizeof(previous_dir)-1] = '\0';
 
+    if (getcwd(current_dir, sizeof(current_dir)) == NULL) current_dir[0] = '\0';
     return 0;
 }
 
 int cmd_pwd(int argc, char **argv) {
     int physical = 0;
-
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-P") == 0) {
-            physical = 1;
-        } else if (strcmp(argv[i], "-L") == 0) {
-            physical = 0;
-        } else if (strcmp(argv[i], "--help") == 0) {
+        if (strcmp(argv[i], "-P") == 0) physical = 1;
+        else if (strcmp(argv[i], "-L") == 0) physical = 0;
+        else if (strcmp(argv[i], "--help") == 0) {
             printf("Usage: pwd [OPTION]...\n");
             printf("Print the name of the current working directory.\n\n");
             printf("Options:\n");
@@ -58,11 +102,8 @@ int cmd_pwd(int argc, char **argv) {
 
     if (physical) {
         char real[1024];
-        if (realpath(current_dir, real)) {
-            printf("%s\n", real);
-        } else {
-            perror("pwd -P error");
-        }
+        if (realpath(current_dir, real)) printf("%s\n", real);
+        else perror("pwd -P error");
     } else {
         printf("%s\n", current_dir);
     }
@@ -80,117 +121,86 @@ int cmd_export(int argc, char **argv) {
         char *eq = strchr(argv[i], '=');
         if (eq) {
             *eq = '\0';
-            const char *var = argv[i];
-            const char *val = eq + 1;
-            if (setenv(var, val, 1) != 0) {
-                perror("export");
-            }
+            if (setenv(argv[i], eq + 1, 1) != 0) perror("export");
         } else {
-            fprintf(stderr, "export: invalid format '%s'\n", argv[i]);
+            const char *val = getenv(argv[i]);
+            if (val) printf("%s=%s\n", argv[i], val);
+            else fprintf(stderr, "export: %s not set\n", argv[i]);
         }
     }
 
+    return 0;
+}
+
+int cmd_history(int argc, char **argv) {
+    const char *home = getenv("HOME");
+    char path[1024];
+    snprintf(path, sizeof(path), "%s/.cvx_history", home ? home : ".");
+
+    FILE *f = fopen(path, "r");
+    if (!f) { perror("No history"); return 1; }
+
+    char lines[1024][1024];
+    int count = 0;
+    while (fgets(lines[count], sizeof(lines[count]), f) && count < 1024) {
+        lines[count][strcspn(lines[count], "\n")] = 0;
+        count++;
+    }
+    fclose(f);
+
+    if (argc == 2 && argv[1][0] == '!') {
+        int index = -1;
+        if (argv[1][1] == '-') index = count - atoi(argv[1]+2);
+        else index = atoi(argv[1]+1) - 1;
+        if (index >= 0 && index < count) {
+            printf("%s\n", lines[index]);
+        } else {
+            fprintf(stderr, "history: invalid index\n");
+        }
+        return 0;
+    }
+
+    for (int i = 0; i < count; i++) printf("%d  %s\n", i+1, lines[i]);
     return 0;
 }
 
 int cmd_help(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-
+    (void)argc; (void)argv;
     printf("\nCVX Shell Help:\n");
     printf("Built-in commands:\n");
     printf("  cd [dir]       - change current directory\n");
-    printf("  pwd [-L|-P|-help]    - print current directory\n");
+    printf("  pwd [-L|-P|-help] - print current directory\n");
     printf("  ls             - list files\n");
-    printf("  history        - show command history (~/.cvx_history)\n");
-    printf("  echo [args]    - print arguments\n");
-    printf("  export [VAR=value] - set environment variable\n");
+    printf("  history [!N]   - show or repeat command history\n");
+    printf("  echo [args]    - print arguments (supports escapes)\n");
+    printf("  export [VAR=value] - set or show environment variable\n");
     printf("  !!             - repeat last command\n");
     printf("  && and |       - command chaining and pipelines\n");
-    printf("  # [comment]      - inline comments\n");
+    printf("  # [comment]    - inline comments\n");
     printf("  cvx --version, cvx -version, cvx -v  - show shell version\n");
-    printf("  help, cvx --help, cvx -help, cvx -h           - show this help message\n");
-    printf("\nExternal commands can be executed as usual.\n");
-
+    printf("  help, cvx --help, cvx -help, cvx -h - show this help message\n\n");
+    printf("External commands can be executed as usual.\n");
     return 0;
 }
 
 int cmd_ls(int argc, char **argv) {
-    bool has_color = false;
-
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--color=auto") == 0) {
-            has_color = true;
-            break;
-        }
-    }
-
     char *args[128];
     int new_argc = argc;
+    bool has_color = false;
 
-    for (int i = 0; i < argc; i++) args[i] = argv[i];
-
-    if (!has_color && argc < 127) {
-        args[new_argc] = "--color=auto";
-        new_argc++;
+    for (int i = 0; i < argc; i++) {
+        args[i] = argv[i];
+        if (strcmp(argv[i], "--color=auto") == 0) has_color = true;
     }
+
+    if (!has_color && new_argc < 127) {
+        args[new_argc++] = "--color=auto";
+    }
+
     args[new_argc] = NULL;
 
     pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork error");
-        return 1;
-    }
-
-    if (pid == 0) {
-        execvp("ls", args);
-        perror("execvp ls error");
-        exit(EXIT_FAILURE);
-    } else {
-        int status;
-        waitpid(pid, &status, 0);
-        return WIFEXITED(status) ? WEXITSTATUS(status) : 1;
-    }
-}
-
-int cmd_history(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-
-    const char *home = getenv("HOME");
-    if (!home) {
-        fprintf(stderr, "history: HOME environment variable not set\n");
-        return 1;
-    }
-
-    char path[1024];
-    snprintf(path, sizeof(path), "%s/.cvx_history", home);
-
-    FILE *f = fopen(path, "r");
-    if (!f) {
-        perror("history: cannot open ~/.cvx_history");
-        return 1;
-    }
-
-    char line[1024];
-    while (fgets(line, sizeof(line), f)) {
-        printf("%s", line);
-    }
-    fclose(f);
-
-    return 0;
-}
-
-int cmd_echo(int argc, char **argv) {
-    for (int i = 1; i < argc; i++) {
-        if (argv[i][0] == '$') {
-            const char *var = getenv(argv[i] + 1);
-            if (var) printf("%s", var);
-        } else {
-            printf("%s", argv[i]);
-        }
-        if (i < argc - 1) printf(" ");
-    }
-    printf("\n");
-    return 0;
+    if (pid < 0) { perror("fork"); return 1; }
+    if (pid == 0) { execvp("ls", args); perror("execvp"); exit(EXIT_FAILURE); }
+    else { int status; waitpid(pid, &status, 0); return WIFEXITED(status) ? WEXITSTATUS(status) : 1; }
 }
