d8168205b5bc56e72143129f9008e45069eb349f
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2025-11-09 17:01:54 +0100

    Updated commands.h

--- src/commands.h
+++ src/commands.h
@@ -2,6 +2,7 @@
 #define COMMANDS_H
 
 int cmd_cd(int argc, char **argv);
+int cmd_ls(int argc, char **argv);
 int cmd_pwd(int argc, char **argv);
 int cmd_echo(int argc, char **argv);
 int cmd_export(int argc, char **argv);
