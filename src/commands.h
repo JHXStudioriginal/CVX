225cc49987d048ca32a73ceb92aa71db49be0de3
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2025-11-09 19:34:48 +0100

    Updated commands.h with EOL 1.0 license

--- src/commands.h
+++ src/commands.h
@@ -1,3 +1,10 @@
+// Copyright (c) [2025] [JHXStudioriginal]
+// All rights reserved. This file is distributed under the Elasna Ownership License 1.0.
+// Preserve author information and file headers. Do not claim ownership of the code.
+// Do not sell this software or any derivative work without explicit permission.
+// Full license: [https://github.com/JHXStudioriginal/CVX-Shell/blob/main/LICENSE]
+
+
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
