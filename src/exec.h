e09205db5fb39b37cfb17ad503769a7c1710fc5d
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2025-11-17 17:11:23 +0100

    Updated exec.h

--- src/exec.h
+++ src/exec.h
@@ -5,12 +5,13 @@
 
 #ifndef EXEC_H
 #define EXEC_H
+
+#include "parser.h"
+#include "commands.h"
+#include "config.h"
+#include "signals.h"
 #include "linenoise.h"
 
-int split_args(const char *line, char *args[], int max_args);
-void free_args(char *args[], int argc);
-void handle_redirection(char *args[], int *argc);
-void replace_alias(char *args[], int *argc);
 int exec_command(char *cmdline);
 int execute_pipeline(char **cmds, int n);
 
