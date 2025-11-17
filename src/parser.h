afc8ce30a79745ce358e82cccca5b80733b3fba7
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2025-11-17 17:10:30 +0100

    Add files via upload

---
+++ src/parser.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2025 JHXStudioriginal
+// This file is part of the Elasna Open Source License v2.
+// All original author information and file headers must be preserved.
+// For full license text, see: [https://github.com/JHXStudioriginal/Elasna-Ownership-License/blob/main/LICENSE]
+
+
+#ifndef PARSER_H
+#define PARSER_H
+
+int split_args(const char *line, char *args[], int max_args);
+void free_args(char *args[], int argc);
+char* unescape_string(const char *s);
+void unescape_args(char *args[], int argc);
+char* expand_tilde(const char *path);
+char* expand_variables(const char *input);
+void replace_alias(char *args[], int *argc);
+void handle_redirection(char *args[], int *argc);
+
+#endif
