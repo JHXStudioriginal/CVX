9b7c8fbc5c60aaf3f570f9e4251417b59e0ac385
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2025-11-19 15:13:35 +0100

    Updated signals.c

--- src/signals.c
+++ src/signals.c
@@ -6,10 +6,55 @@
 
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
 #include "signals.h"
 
 pid_t child_pid = -1;
 
-void sigint_handler(int signo __attribute__((unused))) {
-    if (child_pid > 0) kill(child_pid, SIGINT);
+void sigint_handler(int signo) {
+    (void)signo;
+    if (child_pid > 0)
+        kill(child_pid, SIGINT);
+}
+
+void sigtstp_handler(int signo) {
+    (void)signo;
+    if (child_pid > 0)
+        kill(child_pid, SIGTSTP);
+}
+
+void sigchld_handler(int signo) {
+    int status;
+    pid_t pid;
+    (void)signo;
+
+    while ((pid = waitpid(-1, &status, WNOHANG | WUNTRACED)) > 0) {
+        if (pid == child_pid) {
+            if (WIFEXITED(status) || WIFSIGNALED(status))
+                child_pid = -1;
+        }
+    }
+}
+
+void setup_signals(void) {
+    struct sigaction sigint_sa, sigtstp_sa, sigchld_sa;
+
+    sigint_sa.sa_handler = sigint_handler;
+    sigemptyset(&sigint_sa.sa_mask);
+    sigint_sa.sa_flags = SA_RESTART;
+    sigaction(SIGINT, &sigint_sa, NULL);
+
+    sigtstp_sa.sa_handler = sigtstp_handler;
+    sigemptyset(&sigtstp_sa.sa_mask);
+    sigtstp_sa.sa_flags = SA_RESTART;
+    sigaction(SIGTSTP, &sigtstp_sa, NULL);
+
+    sigchld_sa.sa_handler = sigchld_handler;
+    sigemptyset(&sigchld_sa.sa_mask);
+    sigchld_sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    sigaction(SIGCHLD, &sigchld_sa, NULL);
+
+    signal(SIGQUIT, SIG_IGN);
 }
