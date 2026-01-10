ef61b197108071eeac8cff9a5c40508d56d063a3
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2026-01-10 22:08:54 +0100

    Updated commands.h

--- src/commands.h
+++ src/commands.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 JHXStudioriginal
+// Copyright (c) 2025-2026 JHXStudioriginal
 // This file is part of the Elasna Open Source License v3.
 // All original author information and file headers must be preserved.
 // For full license text, see: [https://github.com/JHXStudioriginal/Elasna-License/blob/main/LICENSE]
